e7849017ecec1566d504bca475d447bc28e283bd
Teja <lovlytejaa@gamil.com>
2022-10-25 18:08:00 +0530

    dark theme

--- src/App.js
+++ src/App.js
@@ -4,17 +4,80 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dashboard from './Pages/Dashboard';
 import CoinPage from './Pages/CoinPage';
 import Compare from './Pages/Compare';
+import { useEffect } from 'react';
 
 function App() {
+  // var cursor1;
+  // var cursor2;
+  // var cursor3;
+
+  // useEffect(() => {
+  //   cursor1 = document.getElementById("cursor1");
+  //   document.body.addEventListener("mousemove", function (e) {
+  //     return (
+  //       (cursor1.style.left = e.clientX + "px"),
+  //       (cursor1.style.top = e.clientY + "px")
+  //     );
+  //   });
+
+  //   cursor2 = document.getElementById("cursor2");
+  //   document.body.addEventListener("mousemove", function (e) {
+  //     return (
+  //       (cursor2.style.left = e.clientX + "px"),
+  //       (cursor2.style.top = e.clientY + "px")
+  //     );
+  //   });
+  //   cursor3 = document.getElementById("cursor3");
+  //   document.body.addEventListener("mousemove", function (e) {
+  //     return (
+  //       (cursor3.style.left = e.clientX + "px"),
+  //       (cursor3.style.top = e.clientY + "px")
+  //     );
+  //   });
+  // document.body.addEventListener("mousedown", function (e) {
+  //   return (
+  //     (cursor.style.height = "0.5rem"),
+  //     (cursor.style.width = "0.5rem"),
+  //     (cursorPointer.style.height = "2.5rem"),
+  //     (cursorPointer.style.width = "2.5rem")
+  //   );
+  // });
+  // document.body.addEventListener("mouseup", function (e) {
+  //   return (
+  //     (cursor.style.height = "0.3rem"),
+  //     (cursor.style.width = "0.3rem"),
+  //     (cursorPointer.style.height = "2rem"),
+  //     (cursorPointer.style.width = "2rem")
+  //   );
+  // });
+  // }, []);
+
+  const setInitialTheme = `
+  function getUserPreference() {
+    if(window.localStorage.getItem('theme')) {
+      return window.localStorage.getItem('theme')
+    }
+    return window.matchMedia('(prefers-color-scheme: light)').matches 
+      ? 'light' 
+      : 'dark'
+  }
+  document.body.dataset.theme = getUserPreference();
+  `;
   return (
-    <BrowserRouter>
+    <>
+      <script dangerouslySetInnerHTML={{ __html: setInitialTheme }} />
+      {/* <div className="cursor1" id="cursor1"></div>
+      <div className="cursor2" id="cursor2"></div>
+      <div className="cursor3" id="cursor3"></div> */}
+      <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/coin' element={<CoinPage />} />
           <Route path='/compare' element={<Compare />} />
         </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/Components/HomePage/Header/Header.js
+++ src/Components/HomePage/Header/Header.js
@@ -1,35 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../../mui_components/Button/Button'
 import './styles.css'
 import TemporaryDrawer from './Drawer';
+import Search from '../../DashboardPage/Search';
+import { Switch } from '@mui/material';
 
 const Header = () => {
-  return (
-    <div className='navbar'>
-        <a href='/'>
-            <h1>Crypto Dashboard.</h1>
-        </a>
-        <div className='links-div'>
+    const setDark = () => {
+        localStorage.setItem("theme", "dark");
+        document.documentElement.setAttribute("data-theme", "dark")
+    }
+    const setLight = () => {
+        localStorage.setItem("theme", "light");
+        document.documentElement.setAttribute("data-theme", "light")
+    }
+    const storedTheme = localStorage.getItem("theme");
+    
+    const prefersDark = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark").matches
+    
+    const defaultDark =
+    storedTheme === "dark" || (storedTheme === null && prefersDark);
+
+    const [darkTheme, setDarkTheme] = useState(
+        defaultDark == "dark" ? true : false
+    );
+
+    if (defaultDark) {
+        setDark();
+    } 
+
+    
+  const toggleTheme = (e) => {
+    if (!darkTheme) {
+      setDark();
+    } else {
+      setLight();
+    }
+    setDarkTheme(!darkTheme);
+  };
+    return (
+        <div className='navbar'>
             <a href='/'>
-                <p className='link'>Home</p>
+                <h1>Crypto Dashboard.</h1>
             </a>
-            <a href='/compare'>
-                <p className='link'>Compare</p>
-            </a>
-            <a href='/about'>
-                <p className='link'>About Us</p>
-            </a>
-            <a href='/dashboard'>
-                <p className='link'>
-                    <Button text={'Dashboard'} className={'button'} />
-                </p>
-            </a>
+            <div className='links-div'>
+                <Switch
+                    defaultChecked
+                    value={!darkTheme}
+                    onClick={() => toggleTheme()}
+                />
+                <a href='/'>
+                    <p className='link'>Home</p>
+                </a>
+                <a href='/compare'>
+                    <p className='link'>Compare</p>
+                </a>
+                <a href='/about'>
+                    <p className='link'>About Us</p>
+                </a>
+                <a href='/dashboard'>
+                    <p className='link'>
+                        <Button text={'Dashboard'} className={'button'} />
+                    </p>
+                </a>
+            </div>
+            <div className='menu-div'>
+                <TemporaryDrawer />
+            </div>
         </div>
-        <div className='menu-div'>
-            <TemporaryDrawer />
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Header
