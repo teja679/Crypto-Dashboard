5e46d369d0abee8a49c5586bc304fbb0fd766d7d
Teja <lovlytejaa@gamil.com>
2022-10-25 12:39:24 +0530

    select coins, list flex

---
+++ src/Components/ComparePage/Select/Select.js
@@ -0,0 +1,36 @@
+import { MenuItem, Select } from '@mui/material'
+import React from 'react'
+
+export const SelectComponent = ({
+    value, handleChange, data, filter
+}) => {
+  return (
+    <div>
+        <Select
+            value={value ?? "crypto"}
+            onChange={handleChange}
+            className="select-days"
+            sx={{
+                height:'2.5rem', color: "var(--white",
+                "& .MuiOutlinedInput-notchedOutline": {
+                    borderColor: "var(--white)",
+                  },
+                  "& .MuiSvgIcon-root": {
+                    color: "var(--white)",
+                  },
+                  "&:hover": {
+                    "&& fieldset": {
+                      borderColor: "#3a80e9",
+                    },
+                  },
+            }}
+        >
+            {data.map((item, i) => (
+                <MenuItem key={i} value={item.id}>
+                    {item.name}
+                </MenuItem>
+            ))}
+        </Select>
+    </div>
+  )
+}
